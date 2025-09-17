16d3f7100a23db23ff84ef52478d80b7add2fcef
a23barho <azim.barhoumi@imt-atlantique.net>
2025-09-17 21:03:51 +0200

    Footer (+CSS)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import Resume from "./components/Resume/Resume";
 
 import {
--- src/components/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default function Footer() {
-  return <h1 style={{ padding: 16 }}>Footer</h1>;
-}
