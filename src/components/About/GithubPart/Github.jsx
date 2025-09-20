07a4b5af094bf9711d40e720cde4b89fa5dab48f
a23barho <azim.barhoumi@imt-atlantique.net>
2025-09-20 17:09:56 +0200

    Toolstack & github calendar

--- src/components/About/GithubPart/Github.jsx
+++ src/components/About/GithubPart/Github.jsx
@@ -1,3 +1,21 @@
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+
+import "./Github.css";
+
 export default function Github() {
-  return <h1 style={{ padding: 16 }}>Github</h1>;
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="heading" style={{ paddingBottom: "20px" }}>
+        Days I <strong className="blue">Code</strong>
+      </h1>
+      <GitHubCalendar
+        username="Aziiiiim"
+        blockSize={15}
+        blockMargin={5}
+        color="#c084f5"
+        fontSize={16}
+      />
+    </Row>
+  );
 }
--- src/components/About/Toolstack/Toolstack.jsx
+++ src/components/About/Toolstack/Toolstack.jsx
@@ -1,3 +1,40 @@
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiDebian,
+  SiEclipseide,
+  SiJetbrains,
+  SiSublimetext,
+  SiPostman,
+  SiGit
+} from "react-icons/si";
+
+import "./Toolstack.css";
+
 export default function Toolstack() {
-  return <h1 style={{ padding: 16 }}>Toolstack</h1>;
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux title="Linux" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDebian title="Debian 12" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiEclipseide title="Eclipse" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJetbrains title="IntelliJ IDEA" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSublimetext title="Sublime Text" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman title="Postman" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGit title="Git" />
+      </Col>
+    </Row>
+  );
 }
